56cfee3fbca63959fdb31a17b025a7063f885d49
kiran <sharmakiran237@gmail.com>
2022-05-09 19:45:57 +0530

    calculate_length

--- LengthCalculate.java
+++ LengthCalculate.java
@@ -1,19 +1,12 @@
 package com.bridgelabz;
 
 public class LengthCalculate {
-
 	public static void main (String[] args) {
-		//point 1
-		double x1 = 4;
-		double y1 = 3;
 
+		int a1 = 4, b1 = 3;
+		int a2 = 8, b2 = 10;
 
-		// point 2
-		double x2 = 8;
-		double y2 = 10;
-
-		// calculate length of line
-		double length = Math.sqrt( ( ( x2 - x1 ) * (  x2 - x1 ) ) + ( y2 - y1 ) * ( y2 - y1 ) ) ;
+		int length = (int)Math.sqrt( ( ( a2 - a1 ) * (  a2 - a1 ) ) + ( b2 - b1 ) * ( b2 - b1 ) ) ;
 		System.out.println("Lenth of Line is :" + length);
 
 	}
